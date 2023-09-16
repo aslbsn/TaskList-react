b9578d289f1cc7e288d23175bd9ab0838f3897fb
aslbsn <112575572+aslbsn@users.noreply.github.com>
2023-09-16 13:41:03 +0300

    Add files via upload

---
+++ src/components/TaskShow.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import TaskCreate from "./TaskCreate"
+
+function TaskShow({ task, onDelete, onUpdate }) {
+  const [showEdit, setShowEdit]=useState(false)
+  const handleDeleteClick=()=>{
+    onDelete(task.id)
+  }
+  const handleEditClick=()=>{
+    setShowEdit(!showEdit)
+  }
+  
+  const  handleSubmit=(id, updatedTitle, updatedTaskDesc)=>{
+    setShowEdit(false);
+    onUpdate(id, updatedTitle, updatedTaskDesc)
+  }
+ 
+    console.log(task);
+    return (<div className="task-show">
+
+      {showEdit ? (
+      <TaskCreate task={task} taskformUpdate={true} onUpdate={handleSubmit}/>
+      ) : (
+      <div>
+        <h3 className="task-title">Your Task</h3>
+<p>{task.title}</p>
+<h3 className="task-title">To Do List</h3>
+<p>{task.taskDesc}</p>
+<div>
+<button className="task-delete" onClick={handleDeleteClick}>Sil</button>
+<button className="task-update" onClick={handleEditClick}>Güncelle</button>
+</div>
+</div>
+)}     
+    </div>
+    )
+  }
+  
+  export default TaskShow;
+  
